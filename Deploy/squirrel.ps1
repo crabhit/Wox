fe15b36cea2fd415d8e44549aa0453f0a09f2d8b
happlebao <baozhi1992@gmail.com>
2016-05-08 00:18:07 +0100

    release path

--- Deploy/squirrel.ps1
+++ Deploy/squirrel.ps1
@@ -9,14 +9,16 @@ Write-Host "Current path: " + $currentPath
 $path = $env:APPVEYOR_BUILD_FOLDER + "\Deploy\wox.nuspec"
 Write-Host "nuspec path: " + $path
 $basePath = $env:APPVEYOR_BUILD_FOLDER + "\Output\Release"
-& nuget pack $path -Version $env:APPVEYOR_BUILD_VERSION -Properties Configuration=Release -BasePath $basePath
+& .\nuget.exe pack $path -Version $env:APPVEYOR_BUILD_VERSION -Properties Configuration=Release -BasePath $basePath
 
 $nupkgPath = $env:APPVEYOR_BUILD_FOLDER + "\Wox." + $env:APPVEYOR_BUILD_VERSION + ".nupkg"
 Write-Host "nupkg path: " + $nupkgPath
 
 $squirrelPath = $env:APPVEYOR_BUILD_FOLDER + "\packages\squirrel*\tools\Squirrel.exe"
 Write-Host "squirrel path: " + $squirrelPath
-& $squirrelPath --releasify $nupkgPath
+$releasesPath = $env:APPVEYOR_BUILD_FOLDER + "\Releases"
+Write-Host "release path: " + $releasesPath
+& $squirrelPath --releasify $nupkgPath --releaseDir $releasesPath
 
 
 Write-Host "===================================================="
@@ -24,6 +26,6 @@ Write-Host "===================================================="
 Write-Host "===================================================="
 
 Write-Host "===================================================="
-$releases = $env:APPVEYOR_BUILD_FOLDER + "\Releases"
-& ls $releases
+
+& ls $releasesPath
 Write-Host "===================================================="

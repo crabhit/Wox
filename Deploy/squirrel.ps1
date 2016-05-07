a54b83d32d97d73ebca1501273be36bfa058e35c
happlebao <baozhi1992@gmail.com>
2016-05-08 00:37:57 +0100

    d

--- Deploy/squirrel.ps1
+++ Deploy/squirrel.ps1
@@ -21,7 +21,7 @@ Write-Host "release path: " + $releasesPath
 $cmd = $squirrelPath + " --releasify " + $nupkgPath + " --releaseDir " + $releasesPath
 Write-Host "cmd: " + $cmd
 
-& $cmd
+& $squirrelPath --releasify $nupkgPath --releaseDir $releasesPath
 
 
 Write-Host "===================================================="

7c1e6a25d86340ccbb3b334e84174860bede91b3
happlebao <baozhi1992@gmail.com>
2016-05-08 00:28:45 +0100

    f

--- Deploy/squirrel.ps1
+++ Deploy/squirrel.ps1
@@ -14,10 +14,13 @@ $basePath = $env:APPVEYOR_BUILD_FOLDER + "\Output\Release"
 $nupkgPath = $env:APPVEYOR_BUILD_FOLDER + "\Wox." + $env:APPVEYOR_BUILD_VERSION + ".nupkg"
 Write-Host "nupkg path: " + $nupkgPath
 
-$squirrelPath = $env:APPVEYOR_BUILD_FOLDER + "\packages\squirrel*\tools\Squirrel.exe"
+$squirrelPath = $env:APPVEYOR_BUILD_FOLDER + "\packages\squirrel.windows.1.4.0\tools\Squirrel.exe"
 Write-Host "squirrel path: " + $squirrelPath
 $releasesPath = $env:APPVEYOR_BUILD_FOLDER + "\Releases"
 Write-Host "release path: " + $releasesPath
+$cmd = $squirrelPath + " --releasify " + $nupkgPath + " --releaseDir " + $releasesPath
+Write-Host "cmd: " + $cmd
+
 & $squirrelPath --releasify $nupkgPath --releaseDir $releasesPath
 
 

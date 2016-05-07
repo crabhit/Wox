4cdc5d58f965440ee2395c462ace6e78841a9515
happlebao <baozhi1992@gmail.com>
2016-05-07 23:15:48 +0100

    fixup

--- Deploy/squirrel.ps1
+++ Deploy/squirrel.ps1
@@ -8,7 +8,7 @@ Write-Host "Current path: " + $currentPath
 
 $path = $env:APPVEYOR_BUILD_FOLDER + "\Deploy\wox.nuspec"
 Write-Host "nuspec path: " + $path
-$basePath = "\Output\Release"
+$basePath = $env:APPVEYOR_BUILD_FOLDER + "\Output\Release"
 & nuget pack $path -Version $env:APPVEYOR_BUILD_VERSION -Properties Configuration=Release -BasePath $basePath
 
 $nupkgPath = "Wox." + $env:APPVEYOR_BUILD_VERSION + ".nupkg"

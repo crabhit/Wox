3b509b060511ab394954df5530f9b74f4ec09b75
happlebao <baozhi1992@gmail.com>
2016-05-08 01:20:36 +0100

    f

--- Deploy/squirrel.ps1
+++ Deploy/squirrel.ps1
@@ -1,8 +1,3 @@
-Write-Host "===================================================="
-& ls
-Write-Host "===================================================="
-
-
 $currentPath = Convert-Path .
 Write-Host "Current path: " + $currentPath
 
@@ -14,22 +9,7 @@ $basePath = $env:APPVEYOR_BUILD_FOLDER + "\Output\Release"
 $nupkgPath = $env:APPVEYOR_BUILD_FOLDER + "\Wox." + $env:APPVEYOR_BUILD_VERSION + ".nupkg"
 Write-Host "nupkg path: " + $nupkgPath
 
-$squirrelPath = $env:APPVEYOR_BUILD_FOLDER + "\packages\squirrel.windows.1.4.0\tools\Squirrel.com"
+# must use Squirrel.com, Squirrel.exe will produce nothing 
+$squirrelPath = $env:APPVEYOR_BUILD_FOLDER + "\packages\squirrel*\tools\Squirrel.com"
 Write-Host "squirrel path: " + $squirrelPath
-$releasesPath = $env:APPVEYOR_BUILD_FOLDER + "\Releases"
-Write-Host "release path: " + $releasesPath
-$cmd = $squirrelPath + " --releasify " + $nupkgPath + " --releaseDir " + $releasesPath
-Write-Host "cmd: " + $cmd
-get-command $squirrelPath
-& $squirrelPath
-& $squirrelPath --releasify $nupkgPath --releaseDir $releasesPath
-
-
-Write-Host "===================================================="
-& ls
-Write-Host "===================================================="
-
-Write-Host "===================================================="
-
-& ls $releasesPath
-Write-Host "===================================================="
+& $squirrelPath --releasify $nupkgPath
